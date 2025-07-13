3566fd534f419b1aad7ce4948ff0fa94ef6415f9
Wilson Blanco <wblanco60768@ufide.ac.cr>
2025-07-13 14:21:22 -0600

    Se agregan atributos a todas las clases excepto a Presupuesto

--- ProyectoFinalWB/src/com/proyectofinalwb/main/Ingreso.java
+++ ProyectoFinalWB/src/com/proyectofinalwb/main/Ingreso.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package proyectofinalwb;
+package com.proyectofinalwb.main;
 
 /**
  *
@@ -12,7 +12,7 @@ public class Ingreso {
     private String id;
     private String tipoDeIngreso;
     private final double CCSS = 0.108;
-    private int impuestoDeRenta;
+    private double impuestoDeRenta;
     private double salarioBruto;
     private double totalImpuesto;
     private double salarioNeto;
@@ -21,6 +21,8 @@ public class Ingreso {
         this.id = id;
         this.tipoDeIngreso = tipoDeIngreso;
         this.salarioBruto = salarioBruto;
+        calcularImpuestos(salarioBruto);
+        calcularSalarioNeto(this.totalImpuesto, salarioBruto);
     }
         
     
@@ -40,11 +42,11 @@ public class Ingreso {
         this.tipoDeIngreso = tipoDeIngreso;
     }
 
-    public int getImpuestoDeRenta() {
+    public double getImpuestoDeRenta() {
         return impuestoDeRenta;
     }
 
-    public void setImpuestoDeRenta(int impuestoDeRenta) {
+    public void setImpuestoDeRenta(double impuestoDeRenta) {
         this.impuestoDeRenta = impuestoDeRenta;
     }
 
@@ -70,6 +72,34 @@ public class Ingreso {
 
     public void setSalarioNeto(double salarioNeto) {
         this.salarioNeto = salarioNeto;
+    }
+    
+    public void calcularImpuestos(double salarioBruto){
+        if(salarioBruto > 922000.00){
+            setImpuestoDeRenta(0.10);
+        }
+        else if(salarioBruto > 923000.00){
+            setImpuestoDeRenta(0.15);
+        }
+        else if(salarioBruto > 923000.00){
+            setImpuestoDeRenta(0.20);
+        }
+        else if(salarioBruto > 923000.00){
+            setImpuestoDeRenta(0.25);
+        }
+        else{
+            setImpuestoDeRenta(0.0);
+        }
+        setTotalImpuesto(((this.salarioBruto*getImpuestoDeRenta())+(this.salarioBruto*this.CCSS)));
+    }
+    public void calcularSalarioNeto(double totalImpuestos, double salarioBruto){
+        setSalarioNeto(this.salarioBruto-getTotalImpuesto());
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Ingreso{" + "id=" + id + ", tipoDeIngreso=" + tipoDeIngreso + ", totalImpuesto=" + totalImpuesto + ", salarioNeto=" + salarioNeto + '}';
     }
     
 }
--- ProyectoFinalWB/src/com/proyectofinalwb/main/Main.java
+++ ProyectoFinalWB/src/com/proyectofinalwb/main/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyectofinalwb;
+package com.proyectofinalwb.main;
 
 /**
  *
@@ -14,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
     }
     
 }
